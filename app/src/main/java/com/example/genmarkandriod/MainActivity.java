ee442dcd2788e3a8e225392cb44ecf5233c15e57
Anastasia Bir <anabir7@gmail.com>
2020-07-06 00:49:28 +0300

    add choose model

--- app/src/main/java/com/example/genmarkandriod/MainActivity.java
+++ app/src/main/java/com/example/genmarkandriod/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.genmarkandriod;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.content.Intent;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
+import java.util.LinkedHashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final LinkedHashMap<String, String> probableCauses = new LinkedHashMap<String, String>(){{
+        put("um5", "ic_battery");
+        put("ms5", "ic_battery");
+    }};;
+    private RecyclerView mRecyclerView;
+    private ProbableCausesAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void changeToMaps(View view) {
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mAdapter = new ProbableCausesAdapter(this, probableCauses);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
+
 
